126f88d51a3afe6ae4571d901a81591cba5b9c82
BBranco97 <barbarabranco1997@gmail.com>
2026-08-12 21:40:45 -0300

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "br.edu.ifsp.scl.prdm.sc3021131.contadorcompose"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "br.edu.ifsp.scl.prdm.sc3021131.contadorcompose"
--- app/src/main/java/br/edu/ifsp/scl/prdm/sc3021131/contadorcompose/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/prdm/sc3021131/contadorcompose/MainActivity.kt
@@ -4,14 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import br.edu.ifsp.scl.prdm.sc3021131.contadorcompose.ui.theme.ContadorComposeTheme
+import androidx.compose.material3.Text as Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +32,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ContadorComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ContadorComposeScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +42,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ContadorComposeScreen(modifier: Modifier = Modifier) {
+    var initialValue by remember { mutableIntStateOf( 0) }
+    var counter by remember { mutableIntStateOf(0) }
+    Column (modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    )    {
+        OutlinedTextField(
+            label= { Text(text = "Initial Value") },
+            modifier = Modifier.fillMaxWidth(),
+            value = initialValue.toString(),
+            onValueChange = { value ->
+                initialValue = value.toIntOrNull() ?: 0
+                counter = initialValue
+            }
+        )
+        Text(
+            text = counter.toString(),
+            fontSize = 48.sp,
+            modifier = modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+        Button(
+            modifier = modifier.fillMaxWidth(),
+            onClick = {counter++}
+        ){
+            Text("Increment")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ContadorComposeScreenPreview() {
     ContadorComposeTheme {
-        Greeting("Android")
+        ContadorComposeScreen(modifier = Modifier)
     }
 }
